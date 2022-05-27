8d29b3da5c56f7e6cc33ace1eeb883138fd91a89
Ehsan sarshar <ehsan.herai@gmail.com>
2022-05-27 19:23:09 +0430

    handle other cases

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,20 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let contents = fs::read_to_string("hello.html").unwrap();
-    let response = format!("HTTP/1.1 OK\r\nContent-Lenght: {}\r\n\r\n{}",
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let response = format!("{}\r\nContent-Lenght: {}\r\n\r\n{}",
+        status_line,
         contents.len(),
         contents
     );
-
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
