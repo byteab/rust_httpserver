c9db8c7c0651249f58f231042ac4b05f5bd56d4c
Ehsan sarshar <ehsan.herai@gmail.com>
2022-05-27 19:45:16 +0430

    simulate sleep and unlimited thread

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::io::prelude::*;
 use std::fs;
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        thread::spawn(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -15,8 +20,12 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
