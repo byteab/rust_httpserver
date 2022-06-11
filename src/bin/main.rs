8b11bb17412f2132f92cf8954080faaf14807eba
Ehsan sarshar <ehsan.herai@gmail.com>
2022-06-11 23:26:16 +0430

    thread pool added

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,13 +3,15 @@ use std::fs;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 use std::time::Duration;
+use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
